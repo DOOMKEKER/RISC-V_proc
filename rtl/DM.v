9e02764e45c99e5161cc88d7835d535acd08f67b
DOOMKEKER <adeptus@protonmail.com>
2020-12-07 13:10:56 +0300

    test

--- rtl/DM.v
+++ rtl/DM.v
@@ -1,16 +1,20 @@
 module DM
   (
-    input           clk_i,
-    input  [31:0]   addr_i,
-    input  [31:0]   wd_i,
-    input           we_i,
-
-    output [31:0]   rd_i
+    input 			   	    clk_i,
+	  input 	[31:0] 	    a_i  ,
+	  input 	[31:0] 	    wd_i ,
+	  input   [2:0]       mem_size_i,
+	  input 			   	    mem_req_i,
+	  input 			   	    mem_we_i,
+	  input 			   	    reset,
+	  output reg	[31:0] 	rd_o
   );
 
-reg [31:0] data [0:63];
+reg [31:0] STATIC [0:511];
+reg [31:0] HEAP 	[0:511];
+reg [31:0] STACK 	[0:511];
 
-initial $readmemb ("E:\\Studies\\3_course\\1_semestr\\FPGA\\labs\\rtl\\data.txt", data);
+//initial $readmemb ("E:\\Studies\\3_course\\1_semestr\\FPGA\\labs\\rtl\\data.txt", data);
 
 assign rd_i = ((addr_i >= 32'h66000000 ) && ( addr_i <= 32'h660000FC )) ? data[ addr_i[7:0] ] : 32'b0;
 
